4af60fbedd18b9f4004b68325dec06f75fbf632e
NurZorlu <nurzada.zholomanova.ch@gmail.com>
2023-09-08 07:59:21 +0300

    first commit

--- js/home_works.js
+++ js/home_works.js
@@ -1,82 +1,31 @@
-// MOVE BLOCK
-const childBlock = document.querySelector('.child_block')
+// GMAIl block
+const gmailInput = document.querySelector("#gmail_input");
+const gmailButton = document.querySelector("#gmail_button");
+const gmailResult = document.querySelector("#gmail_result");
 
-let positionX = 0
-let positionY = 0
+const regExp = /^[\w+\._-]+@gmail\.com$/;
 
-const move = () => {
-    if (positionX < 449 && positionY === 0) {
-        positionX+=2
-        childBlock.style.left = `${positionX}px`
-        setTimeout(move, 10)
-    } else if (positionX >= 449 && positionY < 449) {
-        positionY+=2
-        childBlock.style.top = `${positionY}px`
-        setTimeout(move, 10)
-    } else if (positionX > 0 && positionY > 0) {
-        positionX-=2
-        childBlock.style.left = `${positionX}px`
-        setTimeout(move, 10)
-    } else if (positionX === 0 && positionY > 0) {
-        positionY-=2
-        childBlock.style.top = `${positionY}px`
-        setTimeout(move, 10)
-    }
-}
+gmailButton.onclick = () => {
+  if (regExp.test(gmailInput.value)) {
+    gmailResult.innerHTML = "OK";
+    gmailResult.style.color = "green";
+  } else {
+    gmailResult.innerHTML = "NOT OK";
+    gmailResult.style.color = "red";
+  }
+};
 
-move()
+// Move block
+const parentBlock = document.querySelector(".parent_block");
+const childBlock = document.querySelector(".child_block");
 
-// STOPWATCH
-const minutesBlock = document.querySelector('#minutes'),
-    secondsBlock = document.querySelector('#seconds'),
-    mlSecondsBlock = document.querySelector('#ml-seconds'),
-    startButton = document.querySelector('#start'),
-    stopButton = document.querySelector('#stop'),
-    resetButton = document.querySelector('#reset')
+let num = 0;
 
-let interval
-let minutes = 0
-let seconds = 0
-let mlSeconds = 0
-
-const startTimer = () => {
-    mlSeconds++
-    mlSeconds <= 99 && (mlSecondsBlock.innerHTML = mlSeconds)
-    mlSeconds === 100 && (mlSecondsBlock.innerHTML = '00')
-
-    mlSecondsBlock.innerHTML = `0${mlSeconds}`
-    mlSeconds > 9 && (mlSecondsBlock.innerHTML = mlSeconds)
-    if (mlSeconds > 99) {
-        seconds++
-        secondsBlock.innerHTML = `0${seconds}`
-        mlSeconds = 0
-    }
-    seconds > 9 && (secondsBlock.innerHTML = seconds)
-    if (seconds > 59) {
-        minutes++
-        minutesBlock.innerHTML = `0${minutes}`
-        seconds = 0
-        secondsBlock.innerHTML = `0${seconds}`
-    }
-    minutes > 9 && (minutesBlock.innerHTML = minutes)
-}
-
-startButton.onclick = () => {
-    clearInterval(interval)
-    interval = setInterval(startTimer,  10)
-}
-
-stopButton.onclick = () => {
-    clearInterval(interval)
-}
-
-resetButton.onclick = () => {
-    clearInterval(interval)
-    minutes = 0
-    seconds = 0
-    mlSeconds = 0
-    minutesBlock.innerHTML = '00'
-    secondsBlock.innerHTML = '00'
-    mlSecondsBlock.innerHTML = '00'
-}
-
+let count = () => {
+  if (num < 448) {
+    num++;
+    childBlock.style.left = `${num}px`;
+    setTimeout(count, 5);
+  }
+};
+count();
